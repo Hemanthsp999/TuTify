e039bd84cfd4b5c2474be303214a49dac81105ef
Rajit-125 <rajithebbar125@gmail.com>
2023-12-22 02:45:09 +0530

    added now

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Login() {
+    const [email,setEmail] = useState("");
     const [showPassword, setShowPassword] = useState(false);
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword)
+    }
+    function onClickSubmit(){
+        console.log("email",email);
+        console.log("password",showPassword);
     }
     return (
         <>
@@ -16,13 +21,15 @@ function Login() {
                 <div className=" mx-64 my-80 bg-blue-600 w-96 h-96 rounded-3xl py-4 px-4">
                     <h1 className=" mx-6 my-5 font-bold text-6xl">Welcome</h1>
                     <h1 className=" mx-5 text-2xl font-bold">email</h1>
-                    <input className=" mx-3 my-1 w-80 text-xl rounded-xl font-bold px-5" type="text" placeholder="enter email..." />
+                    <input className=" mx-3 my-1 w-80 text-xl rounded-xl font-bold px-5" type="text" placeholder="enter email..." value={email} onChange={(e)=>setEmail(e.target.value)} />
                     <h2 className=" mx-5 my-1 text-2xl font-bold">password</h2>
                     <div className="relative">
                         <input
                             className="mx-3 w-80 text-xl rounded-xl font-bold px-5"
                             type={showPassword ? 'text' : 'password'}
                             placeholder="enter password..."
+                            value={showPassword}
+                            onChange={(e) => setShowPassword(e.target.value)}
                         />
                         <span
                             className=" absolute top-1 right-4 cursor-pointer "
@@ -33,7 +40,7 @@ function Login() {
                     </div>
                     <Link to="/signup" className=" my-2 mx-20 text-2xl font-bold hover:text-green-500">new user</Link>
                     <Link to="/">
-                        <button className=" my-2 mx-20 text-6xl font-bold bg-red-500 h-20 px-2 py-2 rounded-2xl hover:bg-blue-800 hover:text-white" >Login</button>
+                        <button className=" my-2 mx-20 text-6xl font-bold bg-red-500 h-20 px-2 py-2 rounded-2xl hover:bg-blue-800 hover:text-white" onSubmit={onClickSubmit} >Submit</button>
                     </Link>
                 </div>
             </div>
--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom"
+
 function SignUp(){
         return(
             <>
-            <div>
-                <h1>jakfjiehif</h1>
+            <div className=" grid grid-cols-2 gap-4">
+                <div className=" bg-blue-600 my-40 mx-64 w-48 px-2 py-2 text-5xl rounded-2xl font-bold">
+                    <Link  className=" ">Student</Link>
+                </div>
+                <div className=" bg-blue-600 my-40 mx-64 w-48 px-2 py-2 text-5xl rounded-2xl font-bold">
+                    <Link  className=" ">Teacher</Link>
+                </div>
             </div>
             </>
         )

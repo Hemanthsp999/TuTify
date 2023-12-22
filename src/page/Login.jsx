7e4c9489e573ec5678f74a547d144f802b7941b5
Rajit-125 <rajithebbar125@gmail.com>
2023-12-22 19:40:24 +0530

    added now++

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
                 <div>
                     <img src="./98.jpg" alt="login" className=" my-20 mx-40 " />
                 </div>
-                <div className=" mx-64 my-80 bg-blue-600 w-96 h-96 rounded-3xl py-4 px-4">
+                <div className=" mx-64 my-80 bg-white border-2 w-96 h-96 rounded-3xl py-4 px-4">
                     <h1 className=" mx-6 my-5 font-bold text-6xl">Welcome</h1>
                     <form id="login-form" method="post" onSubmit={(e)=>onClickSubmit(e)} >
                         <h1 className=" mx-5 text-2xl font-bold">email</h1>
